a6b84bc34cc2000cc5e6ce2a336c24a7326280b3
saiharshavarma <saiharshavarma@outlook.com>
2022-04-20 22:02:31 +0530

    Deployment

--- BillingSystem/settings.py
+++ BillingSystem/settings.py
@@ -129,4 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/", "https://pharmavit.herokuapp.com/"]
